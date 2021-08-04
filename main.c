d0501d27f15a279cf1e408830dffd3266f95e844
cryp70m4n <cryp70m4n@gmail.com>
2021-08-04 18:05:09 +0000

    Working

--- main.c
+++ main.c
@@ -3,6 +3,23 @@
 #include <stdbool.h>
 #include <string.h>
 
+
+#define unsigned long long number;
+
+
+
+bool IsArgument(const char * arg1, const char * arg2) {
+	return (strcmp(arg1,arg2)?0:1);
+}
+
 int main(int argc, char ** argv){
+	for (int i=0; i < argc; i++) {
+		if(i >= argc - 1) {
+			printf("-n requires an value\n");
+		}
+		else {
+			strncpy(number, argv[i+1], sizeof(number));
+		}
+	}
 	return 0;
 }
