ebca74ffb437f2da01c63caf90af1f19e78dbdfa
cryp70m4n <cryp70m4n@gmail.com>
2021-08-04 18:42:01 +0000

    Update

--- main.c
+++ main.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include<errno.h>
 
 
+#define print_failure(fmt, args...) fprintf(stderr, "\033[01;31mFailure:\033[0m "fmt, ##args); fflush(stderr);
+#define print_success(fmt, args...) fprintf(stdout, "\033[01;32mSuccess:\033[0m "fmt, ##args); fflush(stdout);
+
+#define TRUE 1
+#define FALSE 0
 
 
 typedef struct _argument {
 	unsigned long long number;
+	bool random;
+	bool n;
+	bool r;
+	bool check;
 } argument;
 
 
@@ -22,14 +32,50 @@ int main(int argc, char ** argv){
 	for (int i=0; i < argc; i++) {
 		if (IsArgument(argv[i], "-n")) {
 			if(i >= argc - 1) {
-				printf("-n requires an value\n");
+				print_failure("-n requires an value\n");
 				return -1;
 			}
 			else {
 				sscanf(argv[i+1], "%d", &num.number);
+				num.n = TRUE;
+			}
+		}
+		
+			if (IsArgument(argv[i], "-r")) {
+				if(i >= argc - 1) {
+					num.random = TRUE;
+					num.r = TRUE;
+					num.number = rand();
+			}
+				else {
+					print_failure("-r doesn't require a value\n");
+				}
+			}
+	}
+
+
+	if(num.n == TRUE || num.r == TRUE) {
+		num.check = TRUE;
+	}
+
+	if(num.number == 0 && num.check == TRUE) {
+		print_failure("Number is zero\n");
+		return -1;
+	}
+	else {
+		if(num.r == TRUE && num.n == TRUE) {
+			print_failure("Can't use both arguments at the same time\n");
+			return -1;
+		}
+
+		else {
+			if(num.r == TRUE) {
+				print_success("Your random number is:%d\n", num.number);
+			}
+			if(num.n == TRUE) {
+				print_success("Your number:%d\n", num.number);
+				return 0;
 			}
 		}
 	}
-	printf("Your number:%d\n", num.number);
-	return 0;
 }
