907a20104ecba34e7130f1f1b4cb854c609f5fb5
cryp70m4n <cryp70m4n@gmail.com>
2021-08-04 18:18:33 +0000

    Update

--- main.c
+++ main.c
@@ -4,22 +4,32 @@
 #include <string.h>
 
 
-#define unsigned long long number;
 
+
+typedef struct _argument {
+	unsigned long long number;
+} argument;
 
 
 bool IsArgument(const char * arg1, const char * arg2) {
 	return (strcmp(arg1,arg2)?0:1);
 }
 
+
+argument num;
+
 int main(int argc, char ** argv){
 	for (int i=0; i < argc; i++) {
-		if(i >= argc - 1) {
-			printf("-n requires an value\n");
-		}
-		else {
-			strncpy(number, argv[i+1], sizeof(number));
+		if (IsArgument(argv[i], "-n")) {
+			if(i >= argc - 1) {
+				printf("-n requires an value\n");
+				return -1;
+			}
+			else {
+				sscanf(argv[i+1], "%d", &num.number);
+			}
 		}
 	}
+	printf("Your number:%d\n", num.number);
 	return 0;
 }
